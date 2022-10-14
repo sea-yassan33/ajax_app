76aee86715ae14a4d6fdca1c1a98d8a890eab2ef
sea-yassan33 <a.yasuda_tr51@me.com>
2022-10-14 13:14:47 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
